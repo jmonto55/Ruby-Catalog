3a7c7d078d87c3e4db1f8d2e9ddbf343d31e0c56
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-04-24 14:01:36 -0400

    Add menu to display

---
+++ lib/modules/menu.rb
@@ -0,0 +1,19 @@
+module Menu
+  def show_menu
+    puts "
+    \n 1 - List all books
+    \n 2 - List all music albums
+    \n 3 - List all movies
+    \n 4 - List of games
+    \n 5 - List all genres (e.g 'Comedy', 'Thriller')
+    \n 6 - List all labels (e.g. 'Gift', 'New')
+    \n 7 - List all authors (e.g. 'Stephen King')
+    \n 8 - List all sources (e.g. 'From a friend', 'Online shop')
+    \n 9 - Add a book
+    \n 10 - Add a music album
+    \n 11 - Add a movie
+    \n 12 - Add a game
+    \n 13 - Exit
+    "
+  end
+end
