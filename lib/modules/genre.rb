179710a4d8c53059196797c8a8a27e4bfed2e319
Uthmanbello <uthmanbello88@gmail.com>
2023-04-25 00:03:17 +0100

    Setup constructor method for Genre class

--- lib/modules/genre.rb
+++ lib/modules/genre.rb
@@ -1,2 +1,11 @@
-module Genre
+require_relative '../lib/item'
+
+class Genre < Item
+  attr_reader :id, :items
+
+  def initialize(name, items)
+    @id = Random.rand(1..100)
+    @name = name
+    @items = items
+  end
 end
