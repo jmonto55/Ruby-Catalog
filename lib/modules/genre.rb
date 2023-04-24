070ed0b22fcc0ef457079e0feb52be6df616dbd0
Uthmanbello <uthmanbello88@gmail.com>
2023-04-25 00:23:48 +0100

    Add add_item property to Genre class

--- lib/modules/genre.rb
+++ lib/modules/genre.rb
@@ -7,6 +7,11 @@ class Genre < Item
     @id = Random.rand(1..100)
     @name = name
     @items = items
-    super()
+    super(false, Date.today.to_s)
+  end
+
+  def add_item(item)
+    @items << item
+    item.genre = self
   end
 end
