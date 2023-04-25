836c4024eb63f1b7f1956f5e60706790a3727e03
jmonto55 <jmonto55@gmail.com>
2023-04-25 16:05:19 -0500

    Fix list authors method

--- lib/modules/list_authors.rb
+++ lib/modules/list_authors.rb
@@ -1,7 +1,7 @@
 module ListAuthors
   def list_all_authors
     puts 'Authors:'
-    @data.authors.each do |author|
+    @data[0].each do |author|
       puts "#{author.first_name} #{author.last_name}"
     end
   end
