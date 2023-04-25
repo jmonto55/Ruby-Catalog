6920e12a2b890115a5935e486b148866fd3191d7
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-04-25 12:46:29 -0400

    Add publish_date property to book constructor

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,13 @@
 class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize(publisher, cover_state)
-    super
+  def initialize(publish_date, publisher, cover_state)
+    super(publish_date)
     @publisher = publisher
     @cover_state = cover_state
   end
+
+  private
 
   def can_be_archived?
     return true if super.can_be_archived? || cover_state == 'bad'
