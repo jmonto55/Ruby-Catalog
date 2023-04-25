743ef528cac7e21676d7adecca366c29d8bb6ac7
jmonto55 <jmonto55@gmail.com>
2023-04-25 01:11:13 -0500

    Fix list methods

--- lib/modules/add_game.rb
+++ lib/modules/add_game.rb
@@ -1,27 +1,27 @@
 module AddGame
-  def add_game
+  def create_game
     puts 'Select Game author by number:'
     @data.authors.each_with_index do |author, index|
-      puts "[#{index}]: Name: #{author.first_name} #{author.last_name}"
+      puts "[#{index}] - Name: #{author.first_name} #{author.last_name}"
     end
     author = gets.chomp.to_i
 
     puts 'Select Game genre by number:'
     @data.genres.each_with_index do |genre, index|
-      puts "[#{index}]: Genre: #{genre.name}"
+      puts "[#{index}] - Genre: #{genre.name}"
     end
     genre = gets.chomp.to_i
 
     puts 'Select Game label by number:'
     @data.labels.each_with_index do |label, index|
-      puts "[#{index}]: Title: #{label.title} Color: #{label.color}"
+      puts "[#{index}] - Title: #{label.title} Color: #{label.color}"
     end
     label = gets.chomp.to_i
 
-    create_game(author, label, genre, @data)
+    create_game_props(author, label, genre, @data)
   end
 
-  def create_game(author, label, genre, data)
+  def create_game_props(author, label, genre, data)
     print 'Multiplayer available? Y/N :'
     multiplayer = gets.chomp.downcase[0..1] == 'y'
     print 'Last played date dd/mm/yyyy :'
--- lib/modules/list_authors.rb
+++ lib/modules/list_authors.rb
@@ -1,5 +1,5 @@
 module ListAuthors
-  def list_authors
+  def list_all_authors
     puts 'Authors:'
     @data.authors.each do |author|
       puts "#{author.first_name} #{author.last_name}"
