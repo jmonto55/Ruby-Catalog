5e230e2b67cf4e1b54ea9ba5feabce1902b651de
Uthmanbello <uthmanbello88@gmail.com>
2023-04-26 16:24:03 +0100

    Edit add_music file

--- lib/modules/add_a_music.rb
+++ lib/modules/add_a_music.rb
@@ -3,13 +3,13 @@ require_relative '../music_album'
 
 module AddMusicAlbum
   def add_a_music
-    puts 'Genre :'
+    print 'Genre : '
     name = gets.chomp
-    puts 'Name :'
+    print 'Name : '
     album_name = gets.chomp
-    print 'On spotify? Y/N :'
+    print 'On spotify? Y/N : '
     on_spotify = gets.chomp.downcase[0..1] == 'y'
-    print 'Album publish date (DD/MM/YYYY) :'
+    print 'Album publish date (DD/MM/YYYY) : '
     publish_date = gets.chomp
     new_genre = Genre.new(name)
     new_album = MusicAlbum.new(album_name, on_spotify, publish_date)
