645bcee4b6e1c8cfca3e6d45118e7c9e47451f61
Uthmanbello <uthmanbello88@gmail.com>
2023-04-26 17:49:02 +0100

    Update music_album class

--- lib/music_album.rb
+++ lib/music_album.rb
@@ -12,4 +12,13 @@ class MusicAlbum < Item
   def can_be_archived?
     super && on_spotify
   end
+
+  def to_hash
+    {
+      name: @name,
+      on_spotify: @on_spotify,
+      publish_date: @publish_date,
+      genre: {name: genre.name}
+    }
+  end
 end
