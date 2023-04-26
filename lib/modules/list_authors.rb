7ab79055108323405424d8c1b5a6c05bcc8dbc5c
jmonto55 <jmonto55@gmail.com>
2023-04-26 11:01:16 -0500

    Fix list authors method to avoid author repetitions

--- lib/modules/list_authors.rb
+++ lib/modules/list_authors.rb
@@ -1,8 +1,15 @@
+require 'set'
+
 module ListAuthors
   def list_all_authors
     puts 'Authors:'
+    authors = Set.new
     @games.each do |game|
-      puts "- #{game.author.first_name} #{game.author.last_name}"
+      authors << "#{game.author.first_name} #{game.author.last_name}"
+    end
+    authors.each do |author|
+      puts "- #{author}"
     end
   end
 end
+
