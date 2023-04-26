4b21594d5bbd31c20939758f8367942bcb412d12
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-04-26 12:30:20 -0400

    Display all labels

--- lib/modules/list_all_labels.rb
+++ lib/modules/list_all_labels.rb
@@ -1,9 +1,19 @@
 require_relative '../label'
 
+require 'set'
+
 module ListAllLabels
   def list_all_labels
-    @data[1].each do |label|
-      puts "#{label.title} #{label.color}"
+    labels = Set.new
+    @books.each do |book|
+      labels << book.label
+    end
+
+    return puts "\nNo label" if labels.empty?
+
+    puts 'Labels: '
+    labels.each do |label|
+      puts "- Title: -> #{label.title} , Color: -> #{label.color}"
     end
   end
 end
