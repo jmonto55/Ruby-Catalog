ed6ec7341f82ef3173b90f38a0dfdd8eacd79b84
Uthmanbello <uthmanbello88@gmail.com>
2023-04-26 19:35:47 +0100

    Update the save_music_albums file

--- lib/modules/preserve_music_albums_data.rb
+++ lib/modules/preserve_music_albums_data.rb
@@ -9,7 +9,7 @@ module MusicAlbumData
   
       JSON.parse(File.read('data/music_albums.json')).map do |music|
         genre = Genre.new(music['genre']['name'])
-        music_album = MusicAlbum.new(music['name'], music['on_spotify'], music['publish_date'])
+        music_album = MusicAlbum.new(music['aname'], music['on_spotify'], music['publish_date'])
         music_album.genre = genre
         music_album
       end
